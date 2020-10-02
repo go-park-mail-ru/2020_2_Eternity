6965f7fd36d0457d1d592b73cf6f3b6e86821537
Pavel <pavel_smir@mail.ru>
2020-10-02 23:10:22 +0300

    Db to config
    qq

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -2,26 +2,22 @@ package main
 
 import (
 	"github.com/go-park-mail-ru/2020_2_Eternity/configs/config"
-	"github.com/go-park-mail-ru/2020_2_Eternity/internal/app/database"
 	"github.com/go-park-mail-ru/2020_2_Eternity/internal/app/server"
 	"log"
 )
 
 func main() {
-	conf := config.NewConfig()
-
-	db := database.New(&conf.Db)
-	if err := db.Open(); err != nil {
+	if err := config.Db.Open(); err != nil {
 		log.Fatal(err)
 		return
 	}
 
-	if err := db.Close(); err != nil {
+	if err := config.Db.Close(); err != nil {
 		log.Fatal(err)
 		return
 	}
 
-	srv := server.New(&conf.Web.Server)
+	srv := server.New(&config.Conf.Web.Server)
 	if err := srv.Run(); err != nil {
 		log.Fatal(err)
 		return
