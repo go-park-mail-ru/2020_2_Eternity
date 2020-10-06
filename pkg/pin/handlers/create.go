33c6d9b618ef186ef56276370016f3edef8772fa
Pavel <pavel_smir@mail.ru>
2020-10-06 23:29:22 +0300

    Redone create pin handler

--- pkg/pin/handlers/create.go
+++ pkg/pin/handlers/create.go
@@ -1,11 +1,14 @@
 package handlers
 
 import (
-	"errors"
+	"encoding/json"
 	"github.com/gin-gonic/gin"
+	"github.com/go-park-mail-ru/2020_2_Eternity/api"
 	"github.com/go-park-mail-ru/2020_2_Eternity/pkg/jwthelper"
 	"github.com/go-park-mail-ru/2020_2_Eternity/pkg/pin/model"
+	h "github.com/go-park-mail-ru/2020_2_Eternity/pkg/user/handlers"
 	"github.com/google/uuid"
+	"log"
 	"net/http"
 	"os"
 	"path/filepath"
@@ -24,11 +27,11 @@ func randomUuid() (string, error) {
 func generateRelPath(filename string) string {
 	fn := []rune(filename)
 
-	const Depth int = 5  // to config
+	const Depth int = 5      // to config
 	const DirNameLen int = 1 // to config
 
 	var realDepth int
-	if Depth * DirNameLen > len(fn) {
+	if Depth*DirNameLen > len(fn) {
 		realDepth = len(fn) / DirNameLen
 	} else {
 		realDepth = Depth / DirNameLen
@@ -43,72 +46,83 @@ func generateRelPath(filename string) string {
 		dirs = append(dirs, string(fn[i:i+DirNameLen]))
 	}
 
-	return strings.Join(dirs, "/") + "/" + filename
+	res := strings.Join(dirs, "/") + "/" + filename
+	log.Print("[generateRelPath]: ", res)
+	return res
 }
 
-
-
-func prepareFileStorage() (filePath string, err error) {
+func prepareFileStorage() (relPath string, err error) {
 	u, err := randomUuid()
 	if err != nil {
 		return "", err
 	}
 
-	relPath := generateRelPath(u)
-	err = os.MkdirAll(filepath.Dir(relPath), os.ModeDir)
+	const StaticDir = "static/img" // config
+
+	relPath = generateRelPath(u)
+	path := StaticDir + "/" + relPath
+	err = os.MkdirAll(filepath.Dir(path), os.ModePerm|os.ModeDir)
 	if err != nil {
 		return "", err
 	}
 
-	const StaticDir = ""  // config
-
-	return StaticDir + "/" + relPath, nil
+	return relPath, nil
 }
-
 
 func CreatePin(c *gin.Context) {
 	claims, ok := c.Get("info")
 	if !ok {
-		c.AbortWithStatusJSON(http.StatusInternalServerError, errors.New("can't get key"))
+		c.AbortWithStatusJSON(http.StatusInternalServerError, h.Error{"can't get key"})
 		return
 	}
 
 	requester, ok := claims.(jwthelper.Claims)
 	if !ok {
-		c.AbortWithStatusJSON(http.StatusBadRequest, errors.New("can't lead claims"))
+		c.AbortWithStatusJSON(http.StatusBadRequest, h.Error{"can't lead claims"})
 		return
 	}
 
-
-	file, err := c.FormFile("file") // ???
+	file, err := c.FormFile("img") // config
 	if err != nil {
-		c.AbortWithStatusJSON(http.StatusBadRequest, err)
+		c.AbortWithStatusJSON(http.StatusBadRequest, h.Error{"[FormFile] :" + err.Error()})
 		return
 	}
 
-	//filename := filepath.Base(file.Filename)
-	filePath, err := prepareFileStorage()
+	relPath, err := prepareFileStorage()
 	if err != nil {
-		c.AbortWithStatusJSON(http.StatusBadRequest, err)
+		c.AbortWithStatusJSON(http.StatusBadRequest, h.Error{"[prepareFileStorage]: " + err.Error()})
 		return
 	}
 
-	if err := c.SaveUploadedFile(file, filePath); err != nil {
-		c.AbortWithStatusJSON(http.StatusBadRequest, err)
+	const StaticDir = "static/img" // config
+
+	if err := c.SaveUploadedFile(file, StaticDir+"/"+relPath); err != nil {
+		c.AbortWithStatusJSON(http.StatusBadRequest, h.Error{"[SaveUploadedFile]: " + err.Error()})
 		return
 	}
 
-	pin := model.Pin{}
-	if err := c.BindJSON(&pin); err != nil {
-		c.AbortWithStatusJSON(http.StatusBadRequest, err)
+	pinApi := api.CreatePinApi{}
+	jsonStr := c.PostForm("data") // config
+	log.Print(jsonStr)
+
+	if err := json.Unmarshal([]byte(jsonStr), &pinApi); err != nil {
+		c.AbortWithStatusJSON(http.StatusBadRequest, h.Error{"[Unmarshal]: " + err.Error()})
 		return
 	}
 
-	pin.UserId = requester.Id
+	pin := model.Pin{
+		Title:   pinApi.Title,
+		Content: pinApi.Content,
+		ImgLink: relPath,
+		UserId:  requester.Id,
+	}
+
 	if err := pin.CreatePin(); err != nil {
-		c.AbortWithStatusJSON(http.StatusBadRequest, err)
+		c.AbortWithStatusJSON(http.StatusBadRequest, h.Error{"[CreatePin]: " + err.Error()})
 		return
 	}
 
-	c.JSON(http.StatusOK, "{}") // return ID?
+	log.Printf("pin{%v %v %v %v %v}", pin.Id, pin.Title, pin.Content, pin.ImgLink, pin.UserId)
+
+	c.JSON(http.StatusOK, "") // return ID?
 }
