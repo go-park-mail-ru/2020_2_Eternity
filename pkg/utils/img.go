313187754c0eca0dafbb38ea718f555e7c80fa59
Pavel <pavel_smir@mail.ru>
2020-12-20 19:21:53 +0300

    Fixed static path

--- pkg/utils/img.go
+++ pkg/utils/img.go
@@ -2,16 +2,17 @@ package utils
 
 import (
 	"github.com/go-park-mail-ru/2020_2_Eternity/configs/config"
-	"net/url"
 	"path/filepath"
 )
 
 func GetUrlImg(imgName string) string {
-	imgUrl := url.URL{
-		Scheme: config.Conf.Web.Server.Protocol,
-		Host:   config.Conf.Web.Server.Host,
-		Path:   filepath.Join(config.Conf.Web.Static.UrlImg, imgName),
-	}
+	//imgUrl := url.URL{
+	//	Scheme: config.Conf.Web.Server.Protocol,
+	//	Host:   config.Conf.Web.Server.Host,
+	//	Path:   filepath.Join(config.Conf.Web.Static.UrlImg, imgName),
+	//}
+	//
+	//return imgUrl.String()
 
-	return imgUrl.String()
+	return filepath.Join(config.Conf.Web.Static.UrlImg, imgName)
 }
