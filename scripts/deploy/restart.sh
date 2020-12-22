59b2a6a3f014734d75299005a899c08d1cd13f05
Pavel <pavel_smir@mail.ru>
2020-12-22 23:33:43 +0300

    Ci fix #5

--- scripts/deploy/restart.sh
+++ scripts/deploy/restart.sh
@@ -8,10 +8,10 @@ sudo systemctl stop $authMsName || { echo "systemctl: can't stop $authMsName"  ;
 sudo systemctl stop $chatMsName || { echo "systemctl: can't stop $chatMsName"  ; exit 1; }
 sudo systemctl stop $searchMsName || { echo "systemctl: can't stop $searchMsName"  ; exit 1; }
 
-sudo cp /home/ubuntu/pinterest-backend/build/bin-ci/api /bin/pinterest/api || { echo "cp error"  ; exit 1; }
-sudo cp /home/ubuntu/pinterest-backend/build/bin-ci/chat /bin/pinterest/chat || { echo "cp error"  ; exit 1; }
-sudo cp /home/ubuntu/pinterest-backend/build/bin-ci/auth /bin/pinterest/auth || { echo "cp error"  ; exit 1; }
-sudo cp /home/ubuntu/pinterest-backend/build/bin-ci/search /bin/pinterest/search || { echo "cp error"  ; exit 1; }
+sudo cp /home/ubuntu/pinterest-backend/bin-ci/api /bin/pinterest/api || { echo "cp error"  ; exit 1; }
+sudo cp /home/ubuntu/pinterest-backend/bin-ci/chat /bin/pinterest/chat || { echo "cp error"  ; exit 1; }
+sudo cp /home/ubuntu/pinterest-backend/bin-ci/auth /bin/pinterest/auth || { echo "cp error"  ; exit 1; }
+sudo cp /home/ubuntu/pinterest-backend/bin-ci/search /bin/pinterest/search || { echo "cp error"  ; exit 1; }
 
 
 sudo systemctl start $authMsName || { echo "systemctl: can't stop $authMsName"  ; exit 1; }
