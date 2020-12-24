5f9d489cd8a7bb1f1d0bc22f7ab7d23be03c84a9
Pavel <pavel_smir@mail.ru>
2020-12-24 16:51:49 +0300

    60.4

--- pkg/microservices/chat/repository/chat_repo_test.go
+++ pkg/microservices/chat/repository/chat_repo_test.go
@@ -39,6 +39,15 @@ var (
 		LastReadMsgId: 3,
 		NewMessages: 5,
 	}
+
+	ms = domainChat.Message{
+		Id: 1,
+		Content: "csdas",
+		ChatId: 2,
+		UserId: 3,
+		UserName: "name",
+		UserAvatarLink: "ava",
+	}
 )
 
 func TestMain(m *testing.M) {
@@ -232,16 +241,265 @@ func TestGetChats(t *testing.T) {
 	assert.Nil(t, err)
 	assert.Nil(t, mock.ExpectationsWereMet())
 
-	// Fail
+	// Err begin
 
-	//mock.ExpectQuery("WITH usr AS ").
-	//	WithArgs(userId, c.Id).
-	//	WillReturnError(fmt.Errorf(""))
-	//
-	//
-	//_, err = r.GetChatById(c.Id, userId)
-	//
-	//assert.NotNil(t, err)
-	//assert.Nil(t, mock.ExpectationsWereMet())
+	mock.ExpectBegin().WillReturnError(fmt.Errorf(""))
+
+	_, err = r.GetUserChats(userId)
+
+	assert.NotNil(t, err)
+	assert.Nil(t, mock.ExpectationsWereMet())
+
+	// Err 1 query
+
+	mock.ExpectBegin()
+	mock.ExpectQuery("SELECT").
+		WithArgs(userId).WillReturnError(fmt.Errorf(""))
+
+	_, err = r.GetUserChats(userId)
+
+	assert.NotNil(t, err)
+	assert.Nil(t, mock.ExpectationsWereMet())
+
+	// Err o rows
+
+	mock.ExpectBegin()
+	mock.ExpectQuery("SELECT").
+		WithArgs(userId).
+		WillReturnRows(sqlmock.NewRows([]string{"COUNT(1)"}).AddRow(0))
+
+	_, err = r.GetUserChats(userId)
+
+	assert.NotNil(t, err)
+	assert.Nil(t, mock.ExpectationsWereMet())
+
+	// Err 2 query
+
+	mock.ExpectBegin()
+	mock.ExpectQuery("SELECT").
+		WithArgs(userId).
+		WillReturnRows(sqlmock.NewRows([]string{"COUNT(1)"}).AddRow(1))
+	mock.ExpectQuery("SELECT c.id").
+		WithArgs(userId).WillReturnError(fmt.Errorf(""))
+
+	_, err = r.GetUserChats(userId)
+
+	assert.NotNil(t, err)
+	assert.Nil(t, mock.ExpectationsWereMet())
+
+
+	// Err commit
+
+	mock.ExpectBegin()
+	mock.ExpectQuery("SELECT").
+		WithArgs(userId).
+		WillReturnRows(sqlmock.NewRows([]string{"COUNT(1)"}).AddRow(1))
+	mock.ExpectQuery("SELECT c.id").
+		WithArgs(userId).
+		WillReturnRows(
+			sqlmock.NewRows([]string{"c.id", "c.creation_time", "c.last_msg_id",
+				"c.last_msg_content", "c.last_msg_username", "c.last_msg_time",
+				"(SELECT username FROM usr)",
+				" u.username", "u.avatar", "uc.last_read_msg_id", "uc.new_messages"}).
+				AddRow(c.Id, c.CreationTime, c.LastMsgId, c.LastMsgContent, c.LastMsgUsername,
+					c.LastMsgTime, c.UserName, c.CollocutorName, c.CollocutorAvatarLink, c.LastReadMsgId, c.NewMessages))
+	mock.ExpectCommit().WillReturnError(fmt.Errorf(""))
+
+	_, err = r.GetUserChats(userId)
+
+	assert.NotNil(t, err)
+	assert.Nil(t, mock.ExpectationsWereMet())
+
+}
+
+func TestMkRead(t *testing.T) {
+	db, mock, e := sqlmock.New()
+	if e != nil {
+		t.Fatalf("an error '%s' was not expected when opening a stub database connection", e)
+	}
+	defer db.Close()
+
+	// Ok
+
+	mock.ExpectExec("UPDATE uu_chat").
+		WithArgs(userId, c.Id).
+		WillReturnResult(sqlmock.NewResult(1, 1))
+
+	r := NewRepo(db)
+	err := r.MarkAllMessagesRead(c.Id, userId)
+
+	assert.Nil(t, err)
+	assert.Nil(t, mock.ExpectationsWereMet())
+
+	// Err
+
+	mock.ExpectExec("UPDATE uu_chat").
+		WithArgs(userId, c.Id).
+		WillReturnError(fmt.Errorf(""))
+
+	err = r.MarkAllMessagesRead(c.Id, userId)
+
+	assert.NotNil(t, err)
+	assert.Nil(t, mock.ExpectationsWereMet())
+}
+
+
+func TestStoreMessage(t *testing.T) {
+	db, mock, e := sqlmock.New()
+	if e != nil {
+		t.Fatalf("an error '%s' was not expected when opening a stub database connection", e)
+	}
+	defer db.Close()
+
+	// Ok
+
+	mock.ExpectQuery("WITH u AS").
+		WithArgs(userId, ms.Content, sqlmock.AnyArg(), ms.ChatId).
+		WillReturnRows(sqlmock.NewRows(
+			[]string{"id", "content", "creation_time", "chat_id", "user_id", "username", "avatar"}).
+			AddRow(ms.Id, ms.Content, ms.CreationTime, ms.ChatId, ms.UserId, ms.UserName, ms.UserAvatarLink))
+
+
+	r := NewRepo(db)
+	_, err := r.StoreMessage(&domainChat.CreateMessageReq{
+		ChatId: ms.ChatId,
+		Content: ms.Content,
+	}, userId)
+
+	assert.Nil(t, err)
+	assert.Nil(t, mock.ExpectationsWereMet())
+
+	// Err
+
+	mock.ExpectQuery("WITH u AS").
+		WithArgs(userId, ms.Content, sqlmock.AnyArg(), ms.ChatId).
+		WillReturnError(fmt.Errorf(""))
+
+
+	_, err = r.StoreMessage(&domainChat.CreateMessageReq{
+		ChatId: ms.ChatId,
+		Content: ms.Content,
+	}, userId)
+
+	assert.NotNil(t, err)
+	assert.Nil(t, mock.ExpectationsWereMet())
+}
+
+
+func TestGetLastN(t *testing.T) {
+	db, mock, e := sqlmock.New()
+	if e != nil {
+		t.Fatalf("an error '%s' was not expected when opening a stub database connection", e)
+	}
+	defer db.Close()
+
+	// Ok
+
+	mock.ExpectQuery("SELECT").
+		WithArgs(ms.Id, 1).
+		WillReturnRows(sqlmock.NewRows(
+			[]string{"id", "content", "creation_time", "chat_id", "user_id", "username", "avatar"}).
+			AddRow(ms.Id, ms.Content, ms.CreationTime, ms.ChatId, ms.UserId, ms.UserName, ms.UserAvatarLink))
+
+	r := NewRepo(db)
+	_, err := r.GetLastNMessages(&domainChat.GetLastNMessagesReq{
+		ChatId: c.Id,
+		NMessages: 1,
+	})
+
+	assert.Nil(t, err)
+	assert.Nil(t, mock.ExpectationsWereMet())
+
+	// Err
+
+	mock.ExpectQuery("SELECT").
+		WithArgs(ms.Id, 1).
+		WillReturnError(fmt.Errorf(""))
+
+	_, err = r.GetLastNMessages(&domainChat.GetLastNMessagesReq{
+		ChatId: c.Id,
+		NMessages: 1,
+	})
+
+	assert.NotNil(t, err)
+	assert.Nil(t, mock.ExpectationsWereMet())
+
+}
+
+func TestGetNBefore(t *testing.T) {
+	db, mock, e := sqlmock.New()
+	if e != nil {
+		t.Fatalf("an error '%s' was not expected when opening a stub database connection", e)
+	}
+	defer db.Close()
+
+	// Ok
+
+	mock.ExpectQuery("SELECT id").
+		WithArgs(ms.Id, 1, 1).
+		WillReturnRows(sqlmock.NewRows(
+			[]string{"id", "content", "creation_time", "chat_id", "user_id", "username", "avatar"}).
+			AddRow(ms.Id, ms.Content, ms.CreationTime, ms.ChatId, ms.UserId, ms.UserName, ms.UserAvatarLink))
+
+	r := NewRepo(db)
+	_, err := r.GetNMessagesBefore(&domainChat.GetNMessagesBeforeReq{
+		ChatId: c.Id,
+		NMessages: 1,
+		BeforeMessageId: 1,
+	})
+
+
+	assert.Nil(t, err)
+	assert.Nil(t, mock.ExpectationsWereMet())
+
+
+	// Err
+
+	mock.ExpectQuery("SELECT id").
+		WithArgs(ms.Id, 1, 1).
+		WillReturnError(fmt.Errorf(""))
+
+	_, err = r.GetNMessagesBefore(&domainChat.GetNMessagesBeforeReq{
+		ChatId: c.Id,
+		NMessages: 1,
+		BeforeMessageId: 1,
+	})
+
+
+	assert.NotNil(t, err)
+	assert.Nil(t, mock.ExpectationsWereMet())
+}
+
+func TestGetCollId(t *testing.T) {
+	db, mock, e := sqlmock.New()
+	if e != nil {
+		t.Fatalf("an error '%s' was not expected when opening a stub database connection", e)
+	}
+	defer db.Close()
+
+	// Ok
+
+	mock.ExpectQuery("SELECT collocutor_id").
+		WithArgs(userId, c.Id).
+		WillReturnRows(sqlmock.NewRows(
+			[]string{"collocutor_id"}).
+			AddRow(1))
+
+	r := NewRepo(db)
+	_, err := r.GetCollocutorId(userId, c.Id)
+
+	assert.Nil(t, err)
+	assert.Nil(t, mock.ExpectationsWereMet())
+
+	// Err
+
+	mock.ExpectQuery("SELECT collocutor_id").
+		WithArgs(userId, c.Id).
+		WillReturnError(fmt.Errorf(""))
+
+	_, err = r.GetCollocutorId(userId, c.Id)
+
+	assert.NotNil(t, err)
+	assert.Nil(t, mock.ExpectationsWereMet())
 
 }
--- pkg/notifications/repository/postgres/notes_test.go
+++ pkg/notifications/repository/postgres/notes_test.go
@@ -2,6 +2,7 @@ package postgres
 
 import (
 	"errors"
+	"fmt"
 	"github.com/DATA-DOG/go-sqlmock"
 	"github.com/go-park-mail-ru/2020_2_Eternity/configs/config"
 	"github.com/go-park-mail-ru/2020_2_Eternity/pkg/domain"
@@ -201,5 +202,40 @@ func TestUpdNotes(t *testing.T) {
 	er := r.UpdateUserNotes(n.Id)
 
 	assert.NotNil(t, er)
+	assert.Nil(t, mock.ExpectationsWereMet())
+}
+
+func TestGetNotesToUser(t *testing.T) {
+	db, mock, e := sqlmock.New()
+	if e != nil {
+		t.Fatalf("an error '%s' was not expected when opening a stub database connection", e)
+	}
+	defer db.Close()
+
+	// Ok
+
+	n := note
+	mock.ExpectQuery("SELECT id").
+		WithArgs(1).
+		WillReturnRows(sqlmock.NewRows([]string{"id", "to_user_id", "type", "encoded_data", "creation_time", "is_read"}).
+			AddRow(n.Id, n.ToUserId, n.Type, n.EncodedData, n.CreationTime, n.IsRead))
+
+	r := NewRepo(db)
+	_, err := r.GetNotesToUser(1)
+
+	assert.Nil(t, err)
+	assert.Nil(t, mock.ExpectationsWereMet())
+
+	// Err
+
+
+	mock.ExpectQuery("SELECT id").
+		WithArgs(1).
+		WillReturnError(fmt.Errorf(""))
+
+
+	_, err = r.GetNotesToUser(1)
+
+	assert.NotNil(t, err)
 	assert.Nil(t, mock.ExpectationsWereMet())
 }
